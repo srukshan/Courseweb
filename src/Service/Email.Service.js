cfd0e9387fe1deb3b57dedbf6aef9bd5ce4bb421
Sasi02 <sazlakshitha@gmail.com>
2019-06-20 11:44:06 +0000

    Email Service Updated

--- src/Service/Email.Service.js
+++ src/Service/Email.Service.js
@@ -1,4 +1,38 @@
+const mailjet = require ('node-mailjet');
+const API_KEY = "c0a4ca364bccb4c37c8026c95e737b3c";
+const SECRET_KEY = "0a3cc1fe693518cf3d7c3ce98c57b02c";
 
 const EmailService = new function() {
-    this.send = (receiver, content )
+    this.send = (receiver, content ) => {
+        mailjet.connect(API_KEY, SECRET_KEY);
+
+        const request = mailjet
+        .post("send", {'version': 'v3.1'})
+        .request({
+            "Messages":[
+                {
+                    "From": {
+                        "Email": "$SENDER_EMAIL",
+                        "Name": "Me"
+                        },
+                    "To": [
+                        {
+                            "Email": "$RECIPIENT_EMAIL",
+                            "Name": "You"
+                            }
+                        ],
+                    "Subject": "My first Mailjet Email!",
+                    "TextPart": "Greetings from Mailjet!",
+                    "HTMLPart": "<h3>Dear passenger 1, welcome to <a href=\"https://www.mailjet.com/\">Mailjet</a>!</h3><br />May the delivery force be with you!"
+                }
+            ]
+        });
+
+        request.then((result) => {
+            console.log(result.body)
+        })
+        .catch((err) => {
+            console.log(err.statusCode)
+        })
+    }
 }
