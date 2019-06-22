78f31327d456db10447b6c6fb1ce313b62a8d2be
sachithanjula <41498657+sachithanjula@users.noreply.github.com>
2019-06-22 10:49:46 +0530

    Routes connect with UI step 1

--- src/Routes/User.Route.js
+++ src/Routes/User.Route.js
@@ -1,15 +1,18 @@
-const router = require('express').Router()
-const controller = require('../Controller/User.Controller')
+const router = require("express").Router();
+const controller = require("../Controller/User.Controller");
 
-router.route('/')
-    .post((req, res) => {
-        controller.insert(req.body)
-            .then(data => res.status(data.status).send(data.message))
-            .catch(err => res.status(err.status).send(err.message))
-    })
-    .get((req, res) => {
-        controller.getAll()
-            .then(data => res.status(data.status).json(data.data))
-            .catch(err => res.status(err.status).send(err.message))
-    })
-module.exports = router
+router
+  .route("/")
+  .post((req, res) => {
+    controller
+      .insert(req.body)
+      .then(data => res.status(data.status).send(data.message))
+      .catch(err => res.status(err.status).send(err.message));
+  })
+  .get((req, res) => {
+    controller
+      .getAll()
+      .then(data => res.status(data.status).json(data.data))
+      .catch(err => res.status(err.status).send(err.message));
+  });
+module.exports = router;
