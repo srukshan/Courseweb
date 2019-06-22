deced7715e1182cf096e1c28ab36d2da2a9cf2d4
sachithanjula <41498657+sachithanjula@users.noreply.github.com>
2019-06-22 23:20:49 +0530

    Backend frontend integration

--- RestApp.js
+++ RestApp.js
@@ -1,10 +1,15 @@
 const Express = require("express")
 const app = Express()
 const BodyParser = require("body-parser")
+
 const ClientRoute = require('./src/Routes/Client.Route')
 const UserRoute = require('./src/Routes/User.Route')
+
 const CourseRoute = require('./src/Routes/Course.Route')
 const AssessmentRoute = require('./src/Routes/Assessment.Route')
+
+const UserCourse = require('./src/Routes/UserCourse.Route')
+
 const Passport = require('passport')
 const Auth = require('./src/Controller/Authentication.Controller')
 
@@ -19,5 +24,6 @@ app.use('/clients/', Auth.isAuthenticated, ClientRoute);
 app.use('/users/', UserRoute);
 app.use('/courses/', CourseRoute);
 app.use('/assessments/', AssessmentRoute);
+app.use('/usercourse/', UserCourse)
 
 module.exports = app;
--- src/Controller/Course.Controller.js
+++ src/Controller/Course.Controller.js
@@ -10,7 +10,7 @@ var NotificationSchema = require("../Schema/Notification.Schema");
 // /api/courses                 POST        { name }        auth admin
 // /api/courses/{id}/accept     POST                        auth instructor
 
-module.exports = new(function () {
+module.exports = new (function() {
   this.insert = data => {
     return new Promise(async (resolve, reject) => {
       var Course = new CourseSchema({
@@ -21,6 +21,30 @@ module.exports = new(function () {
       Course.save()
         .then(course => {
           id = course._id;
+          console.log(id);
+          let instructors = data.instructors;
+
+          instructors.forEach(instructor => {
+            var UserCourse = new UserCourseSchema({
+              userId: id,
+              courseId: instructor
+            });
+            UserCourse.save();
+          });
+          console.log("awaa2");
+          let notify = new NotificationSchema({
+            userIds: instructors,
+            title: "New Course Assigned",
+            content: `<p> You have been assigned the ${
+              data.name
+            }. Please click the button below to accept</p>`,
+            url: `api/courses/accept/${id}`,
+            timeStamp: new Date()
+          });
+
+          notify.save().catch(err => {
+            console.error("Error Save Notify : " + err);
+          });
         })
         .catch(err => {
           reject({
@@ -28,40 +52,14 @@ module.exports = new(function () {
             message: err
           });
         });
-
-      let instructors = data.instructors;
-
-      instructors.forEach(instructor => {
-        var UserCourse = new UserCourseSchema({
-          userId: id,
-          courseId: instructor
-        });
-        UserCourse.save().catch(err => {
-          console.error("Error save UserCourse " + instructor + " : " + err);
-        });
-      });
-
-      let notify = new NotificationSchema({
-        userIds: instructors,
-        title: "New Course Assigned",
-        content: `<p> You have been assigned the ${
-          data.name
-        }. Please click the button below to accept</p>`,
-        url: `api/courses/accept/${id}`,
-        timeStamp: new Date()
-      });
-
-      notify.save().catch(err => {
-        console.error("Error Save Notify : " + err);
-      });
     });
   };
   this.accept = (user, id) => {
     return new Promise((res, rej) => {
       UserCourseSchema.findOne({
-          userId: user._id,
-          courseId: id
-        })
+        userId: user._id,
+        courseId: id
+      })
         .exec()
         .then(uc => {
           if (uc == null) {
@@ -114,11 +112,12 @@ module.exports = new(function () {
   };
   this.update = (id, data) => {
     return new Promise((resolve, reject) => {
-      CourseSchema.update({
-            _id: id
-          },
-          data
-        )
+      CourseSchema.update(
+        {
+          _id: id
+        },
+        data
+      )
         .then(() => {
           resolve({
             status: 200,
@@ -173,35 +172,40 @@ module.exports = new(function () {
     return new Promise((resolve, reject) => {
       UserCourseSchema.find({
         userId: id
-      }).exec().then(userCourses => {
-        if (userCourses.length == 0) {
-          reject({
-            status: 404,
-            message: "no Courses found"
-          })
-        }
-        let courses = [];
-        userCourses.forEach(userCourse => {
-          CourseSchema.findById(userCourse.get('courseId')).exec().then(course => {
-            courses.push(course)
-          })
-        });
-        if (courses.length == 0) {
-          reject({
-            status: 404,
-            message: "no Courses found"
-          })
-        }
-        resolve({
-          status: 200,
-          data: courses
-        })
-      }).catch(err => {
-        reject({
-          status: 500,
-          message: "Error Finding Course : " + err
-        })
       })
+        .exec()
+        .then(userCourses => {
+          if (userCourses.length == 0) {
+            reject({
+              status: 404,
+              message: "no Courses found"
+            });
+          }
+          let courses = [];
+          userCourses.forEach(userCourse => {
+            CourseSchema.findById(userCourse.get("courseId"))
+              .exec()
+              .then(course => {
+                courses.push(course);
+              });
+          });
+          if (courses.length == 0) {
+            reject({
+              status: 404,
+              message: "no Courses found"
+            });
+          }
+          resolve({
+            status: 200,
+            data: courses
+          });
+        })
+        .catch(err => {
+          reject({
+            status: 500,
+            message: "Error Finding Course : " + err
+          });
+        });
     });
   };
   this.delete = id => {
